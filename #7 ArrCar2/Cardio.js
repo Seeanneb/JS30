8315f75a8cf63747d215a0f42e00d292880bd440
PASS <seeanneb@gmail.com>
2018-05-22 12:38:23 -0700

    finished #7

---
+++ #7 ArrCar2/Cardio.js
@@ -0,0 +1,46 @@
+
+    const people = [
+        { name: 'Wes', year: 1988 },
+        { name: 'Kait', year: 1986 },
+        { name: 'Irv', year: 1970 },
+        { name: 'Lux', year: 2015 }
+      ];
+  
+      const comments = [
+        { text: 'Love this!', id: 523423 },
+        { text: 'Super good', id: 823423 },
+        { text: 'You are the best', id: 2039842 },
+        { text: 'Ramen is my fav food ever', id: 123523 },
+        { text: 'Nice Nice Nice!', id: 542328 }
+      ];
+  
+      const isAdult = people.some(x => ((new Date())
+      .getFullYear()) - x.year >= 19);
+      
+      console.log(isAdult)
+
+      const allAdult = people.every(x => ((new Date())
+      .getFullYear()) - x.year >= 19);
+      
+      console.log(allAdult)
+
+      // Some and Every Checks
+      // Array.prototype.some() // is at least one person 19 or older?
+      // Array.prototype.every() // is everyone 19 or older?
+  
+      const finder = comments.find(x =>
+          x.id === 823423 )
+      console.log(finder)
+      // Array.prototype.find()
+      // Find is like filter, but instead returns just the one you are looking for
+      // find the comment with the ID of 823423
+
+      const indexer = comments.findIndex(x => 
+          x.id === 823423 )
+      console.log(indexer)
+      comments.splice(indexer, 1)
+        console.log(comments)
+  
+      // Array.prototype.findIndex()
+      // Find the comment with this ID
+      // delete the comment with the ID of 823423
