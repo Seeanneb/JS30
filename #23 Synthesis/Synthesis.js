0ddac37a5258df0bee6f91c43a489ffb658cb32d
PASS <seeanneb@gmail.com>
2018-06-07 16:01:56 -0700

    working on 23

---
+++ #23 Synthesis/Synthesis.js
@@ -0,0 +1,14 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices(){
+    voices = this.getVoices
+    console.log(voices)
+}
+
+speechSynthesis.addEventListener('voicechanged', populateVoices);
